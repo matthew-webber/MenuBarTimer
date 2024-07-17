cd28e6f19fcfa1426d987d560c9a6e7d87b05a54
Matt Webber <dev.matthew.lee.webber@gmail.com>
2024-07-17 11:32:44 -0400

    first commit

--- MenuBarTimer/MenuBarTimerApp.swift
+++ MenuBarTimer/MenuBarTimerApp.swift
@@ -1,17 +1,62 @@
-//
-//  MenuBarTimerApp.swift
-//  MenuBarTimer
-//
-//  Created by m on 7/17/24.
-//
-
 import SwiftUI
 
 @main
 struct MenuBarTimerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+    var timerView: TimerView!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the status item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "clock", accessibilityDescription: "Timer")
+            button.action = #selector(showMenu)
+        }
+
+        timerView = TimerView()
+        constructMenu()
+    }
+
+    @objc func showMenu() {
+        if let button = statusItem.button {
+            let menu = NSMenu()
+
+            let timerViewItem = NSHostingView(rootView: timerView)
+            timerViewItem.frame = NSRect(x: 0, y: 0, width: 200, height: 100)
+            let customMenuItem = NSMenuItem()
+            customMenuItem.view = timerViewItem
+
+            menu.addItem(customMenuItem)
+            menu.addItem(NSMenuItem.separator())
+            menu.addItem(NSMenuItem(title: "Pause Timer", action: #selector(pauseTimer), keyEquivalent: "p"))
+            menu.addItem(NSMenuItem(title: "End Timer", action: #selector(endTimer), keyEquivalent: "e"))
+            menu.addItem(NSMenuItem.separator())
+            menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+            statusItem.menu = menu
+            statusItem.button?.performClick(nil)
+        }
+    }
+
+    func constructMenu() {
+        // This method remains empty since we create the menu dynamically in showMenu()
+    }
+
+    @objc func pauseTimer() {
+        timerView.pauseTimer()
+    }
+
+    @objc func endTimer() {
+        timerView.endTimer()
     }
 }
---
+++ MenuBarTimer/TimerView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+struct TimerView: View {
+    @State private var minutes: String = ""
+    @State private var timeRemaining: Int?
+    @State private var timer: Timer?
+
+    var body: some View {
+        VStack {
+            TextField("Minutes", text: $minutes, onCommit: startTimer)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 100)
+                .onAppear {
+                    DispatchQueue.main.async {
+                        NSApplication.shared.mainWindow?.makeFirstResponder(nil)
+                        NSApplication.shared.windows.first?.makeFirstResponder(nil)
+                    }
+                }
+
+            if let timeRemaining = timeRemaining {
+                Text(timeString(time: timeRemaining))
+                    .font(.headline)
+            }
+        }
+        .padding()
+        .frame(width: 200, height: 100)
+    }
+
+    func startTimer() {
+        guard let minutes = Int(minutes) else { return }
+        timeRemaining = minutes * 60
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            updateTimer()
+        }
+    }
+
+    func updateTimer() {
+        if let timeRemaining = self.timeRemaining, timeRemaining > 0 {
+            self.timeRemaining = timeRemaining - 1
+        } else {
+            self.timer?.invalidate()
+            self.timeRemaining = nil
+        }
+    }
+
+    func pauseTimer() {
+        timer?.invalidate()
+    }
+
+    func endTimer() {
+        timer?.invalidate()
+        timeRemaining = nil
+    }
+
+    func timeString(time: Int) -> String {
+        let minutes = time / 60
+        let seconds = time % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
