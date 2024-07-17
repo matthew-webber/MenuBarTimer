064a1378e95d3a9e9b62e169da428dfefe9e43df
Matt Webber <dev.matthew.lee.webber@gmail.com>
2024-07-17 11:00:56 -0400

    Initial Commit

---
+++ MenuBarTimer/MenuBarTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarTimerApp.swift
+//  MenuBarTimer
+//
+//  Created by m on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
